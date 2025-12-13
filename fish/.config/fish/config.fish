5f046160e6f77760e62d8b74faa22c5001a522c8
David Sttivend <dsangel@gmail.com>
2025-12-13 13:17:51 -0500

    Update config to use stow structure

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,13 +1,6 @@
 set EDITOR "nvim"
 set -U fish_greeting
 
-set -x LC_ALL en_US.UTF-8
-set ANDROID_HOME $HOME/Library/Android/sdk
-set PATH $ANDROID_HOME/emulator $PATH
-set PATH $ANDROID_HOME/tools $PATH
-set PATH $ANDROID_HOME/tools/bin $PATH
-set PATH $ANDROID_HOME/platform-tools $PATH
-
 set SPACEFISH_TIME_SHOW false
 set SPACEFISH_USER_SHOW always
 set SPACEFISH_PACKAGE_SHOW false	
@@ -22,15 +15,6 @@ set SPACEFISH_DOCKER_SHOW false
 set SPACEFISH_USER_COLOR F37F97
 set SPACEFISH_DIR_COLOR 79E6F3
 set SPACEFISH_GIT_STATUS_COLOR F37F97
-
-# The Real Real
-set -x VAULT_ADDR https://vault.therealreal.com
-set -x VAULT_SKIP_VERIFY true
-
-set -x GOPATH ~/.go
-
-fish_add_path ~/.go
-fish_add_path ~/.go/bin
 
 alias vim='nvim'
 
@@ -58,7 +42,19 @@ alias dip='docker image prune'
 
 alias flushdns="sudo dscacheutil -flushcache; sudo killall -HUP mDNSResponder"
 
-#alias pj='cd (find ~/workspace -type d -not -path "*/node_modules/*" -maxdepth 1 | fzf) && tn'
+# ASDF configuration code
+if test -z $ASDF_DATA_DIR
+    set _asdf_shims "$HOME/.asdf/shims"
+else
+    set _asdf_shims "$ASDF_DATA_DIR/shims"
+end
+
+# Do not use fish_add_path (added in Fish 3.2) because it
+# potentially changes the order of items in PATH
+if not contains $_asdf_shims $PATH
+    set -gx --prepend PATH $_asdf_shims
+end
+set --erase _asdf_shims
 
 function pj
     # Use fzf to select a directory from ~/workspace excluding node_modules
@@ -167,37 +163,7 @@ function cdwhich
   cd (dirname (which $argv))
 end
 
-function __nvm_use_on_cd --on-variable PWD --description 'Use Node.js version specified by project'
-    if test -e .nvmrc
-       nvm use
-       return
-    end
-    
-    if test -e .node-version
-       nvm use (cat .node-version)
-       return
-    end
-end
-
 function mux
   tmuxinator $argv
 end 
 
-function remove_path
-  if set -l index (contains -i "$argv" $fish_user_paths)
-    set -e fish_user_paths[$index]
-    echo "Removed $argv from the path"
-  end
-end
-
-set -gx direnv_fish_mode disable_arrow
-
-direnv hook fish | source
-
-set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin $PATH /Users/david.sttivend/.ghcup/bin # ghcup-env
-
-source /opt/homebrew/opt/asdf/libexec/asdf.fish
-
-alias aws_trr_legacy="docker run --rm -v ~/.okta/config.legacy.properties:/root/.okta/config.properties -v ~/.aws:/root/.aws -it public.ecr.aws/e4w7r6f6/trr/okta-awscli-java withokta aws trr-legacy --version && export AWS_PROFILE=trr-legacy"
-alias aws_trr_staging="docker run --rm -v ~/.okta/config.staging.properties:/root/.okta/config.properties -v ~/.aws:/root/.aws -it public.ecr.aws/e4w7r6f6/trr/okta-awscli-java withokta aws trr-staging --version && export AWS_PROFILE=trr-staging"
-alias aws_trr_production='docker run --rm -v ~/.okta/config.production.properties:/root/.okta/config.properties -v ~/.aws:/root/.aws -it public.ecr.aws/e4w7r6f6/trr/okta-awscli-java withokta aws trr-production --version && export AWS_PROFILE=trr-production'
