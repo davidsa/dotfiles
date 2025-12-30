567a6a95f14f03cd9d43b1cd4c41e99db4cc4c1b
David Sttivend <dsangel@gmail.com>
2025-12-30 11:53:52 -0500

    update harpoon keybindings

--- nvim/.config/nvim/lua/david/plugins/harpoon.lua
+++ nvim/.config/nvim/lua/david/plugins/harpoon.lua
@@ -1,13 +1,13 @@
 return {
-	"ThePrimeagen/harpoon",
-	config = function()
-		local mark = require("harpoon.mark")
-		local ui = require("harpoon.ui")
+  "ThePrimeagen/harpoon",
+  config = function()
+    local mark = require("harpoon.mark")
+    local ui = require("harpoon.ui")
 
-		vim.keymap.set("n", "<leader>a", mark.add_file)
-		vim.keymap.set("n", "€", ui.toggle_quick_menu)
+    vim.keymap.set("n", "<leader>a", mark.add_file)
+    vim.keymap.set("n", "<M-e>", ui.toggle_quick_menu)
 
-		vim.keymap.set("n", "ˇ", ui.nav_prev)
-		vim.keymap.set("n", "¯", ui.nav_next)
-	end,
+    vim.keymap.set("n", "<M-K>", ui.nav_prev)
+    vim.keymap.set("n", "<M-J>", ui.nav_next)
+  end,
 }
