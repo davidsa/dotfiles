5cbb5afc8da00cb1b8457c74ecfcbb84b08bdf57
David Sttivend <dsangel@gmail.com>
2025-12-30 12:44:30 -0500

    update wifi script

--- waybar/.config/waybar/scripts/wifi-menu.fish
+++ waybar/.config/waybar/scripts/wifi-menu.fish
@@ -5,19 +5,28 @@ if test (nmcli -t -f WIFI g) != "enabled"
     nmcli radio wifi on
 end
 
-# Get Wi-Fi list
+# Get Wi-Fi list with frequency
 set networks (
-    nmcli -t -f IN-USE,SECURITY,SSID,SIGNAL dev wifi list |
+    nmcli -t -f IN-USE,SECURITY,SSID,SIGNAL,FREQ dev wifi list |
     awk -F: '
     {
+        # Used mark
         used = ($1 == "*") ? "✓" : " "
+        # Security icon
         secure = ($2 == "--") ? "󰤨" : "󰤪"
-        printf "%s\t%s\t%s\t%s%%\n", used, secure, $3, $4
+        # Frequency to band
+        if ($5 <= 2484) {
+            band = "2.4G"
+        } else {
+            band = "5G"
+        }
+        # Print menu line
+        printf "%s\t%s\t%s\t%s%%\t%s\n", used, secure, $3, $4, band
     }'
 )
 
 # Show menu
-set choice (printf "%s\n" $networks | wofi --dmenu -i -W 400 -H 200 -p "Wi-Fi")
+set choice (printf "%s\n" $networks | wofi --dmenu -i -W 500 -H 200 -p "Wi-Fi")
 
 # Exit if nothing selected
 test -z "$choice"; and exit 0
